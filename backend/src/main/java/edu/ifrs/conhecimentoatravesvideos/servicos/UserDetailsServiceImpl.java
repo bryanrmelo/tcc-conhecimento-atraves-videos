d9ca3b21a8ffcfe3d6d28213905dde1bbd95c3c5
Bryan Melo <bryanrmelo@gmail.com>
2022-07-19 09:26:07 -0300

    Configs

---
+++ backend/src/main/java/edu/ifrs/conhecimentoatravesvideos/servicos/UserDetailsServiceImpl.java
@@ -0,0 +1,21 @@
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    @Autowired
+    private FuncionarioRepositorio funcionarioRepositorio;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        Funcionario funcionario = funcionarioRepositorio.findByEmail(email)
+            .orElseThrow(() -> new UsernameNotFoundException("Usuário não encontrado"));
+
+        return new UserDetailsImpl(funcionario);
+    }
+    
+}
