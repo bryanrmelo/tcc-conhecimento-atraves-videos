64cff7652b78fc54ecbc8601954ff9d4e0b8773a
Bryan Melo <bryanrmelo@gmail.com>
2022-07-19 08:19:36 -0300

    Mudança para Spring

---
+++ backend/src/main/java/edu/ifrs/conhecimentoatravesvideos/ConhecimentoAtravesVideosApplication.java
@@ -0,0 +1,13 @@
+package edu.ifrs.conhecimentoatravesvideos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConhecimentoAtravesVideosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConhecimentoAtravesVideosApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/edu/ifrs/conhecimentoatravesvideos/ConhecimentoAtravesVideosApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.ifrs.conhecimentoatravesvideos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConhecimentoAtravesVideosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
