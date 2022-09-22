60b731a0ab069974ad1ef48fff747835536fda04
Bryan Melo <bryanrmelo@gmail.com>
2022-09-22 10:04:22 -0300

    Login

--- backend/src/main/java/edu/ifrs/conhecimentoatravesvideos/ConhecimentoAtravesVideosApplication.java
+++ backend/src/main/java/edu/ifrs/conhecimentoatravesvideos/ConhecimentoAtravesVideosApplication.java
@@ -2,9 +2,16 @@ package edu.ifrs.conhecimentoatravesvideos;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
+@EnableJpaRepositories(basePackages = "edu.ifrs.conhecimentoatravesvideos.repositorios")
+@ComponentScan(basePackages = { "edu.ifrs.conhecimentoatravesvideos" })
+@EnableTransactionManagement
+@EntityScan(basePackages = "edu.ifrs.conhecimentoatravesvideos.model")
+@SpringBootApplication
 public class ConhecimentoAtravesVideosApplication {
 
 	public static void main(String[] args) {
--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,3 +1,4 @@
+import { LoginComponent } from './components/login/login.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/home';
@@ -6,6 +7,7 @@ import { SidebarComponent } from './components/sidebar/sidebar.component';
 const routes: Routes = [
   { path: '', component: HomeComponent},
   { path: 'sidebar', component: SidebarComponent },
+  { path: 'login', component: LoginComponent},
   { path: '**', redirectTo: '' }
 ];
 
